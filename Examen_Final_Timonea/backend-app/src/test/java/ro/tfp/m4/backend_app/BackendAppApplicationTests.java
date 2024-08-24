9edfdf57457356b077724815e6e02d5c163209d5
Rassssven <dtimonea@gmail.com>
2024-08-24 10:52:21 +0300

    Inittial commit
    Inittial commit

---
+++ Examen_Final_Timonea/backend-app/src/test/java/ro/tfp/m4/backend_app/BackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.tfp.m4.backend_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
